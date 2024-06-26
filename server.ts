df0b49122f0808ced54c0e84f31627de0b31573e
David <zurek.dav@gmail.com>
2024-06-27 00:13:54 +0200

    Add start message

--- server.ts
+++ server.ts
@@ -3,7 +3,15 @@ import "dotenv/config"
 
 export const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.start(ctx => ctx.reply("Welcome"))
+bot.start(async ctx => {
+	let chat = await bot.telegram.getChat(ctx.chat.id)
+	if (chat.type != "private") return
+
+	ctx.reply(
+		`Hey ${chat.first_name} 👋\n\nWe are still working on building this bot and making it great! Please have some patience, we will send you a message once you can start using this bot! ✌️`
+	)
+})
+
 bot.launch()
 
 process.once("SIGINT", () => bot.stop("SIGINT"))
