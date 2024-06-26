08089a7a1d9fe112bd27192379287da3a497f9a8
David <zurek.dav@gmail.com>
2024-06-26 23:47:39 +0200

    Add scripts for building & running the project

--- server.ts
+++ server.ts
@@ -1,1 +1,10 @@
 import { Telegraf } from "telegraf"
+import "dotenv/config"
+
+export const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
+
+bot.start(ctx => ctx.reply("Welcome"))
+bot.launch()
+
+process.once("SIGINT", () => bot.stop("SIGINT"))
+process.once("SIGTERM", () => bot.stop("SIGTERM"))
