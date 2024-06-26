5a3debd3b2a71680e0c06190c1a5049f0471be40
David <zurek.dav@gmail.com>
2024-06-27 00:34:05 +0200

    Implement saving user on start command

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
+import { PrismaClient } from "@prisma/client"
 import { Telegraf } from "telegraf"
 import "dotenv/config"
 
+export const prisma = new PrismaClient()
 export const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.start(async ctx => {
@@ -10,6 +12,18 @@ bot.start(async ctx => {
 	ctx.reply(
 		`Hey ${chat.first_name} 👋\n\nWe are still working on building this bot and making it great! Please have some patience, we will send you a message once you can start using this bot! ✌️`
 	)
+
+	// Check if the user is already in the database
+	let user = await prisma.user.findFirst({ where: { chatId: chat.id } })
+
+	if (user == null) {
+		// Create a new user
+		await prisma.user.create({
+			data: {
+				chatId: chat.id
+			}
+		})
+	}
 })
 
 bot.launch()
